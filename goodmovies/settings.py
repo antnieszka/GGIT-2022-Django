38313d41664fc99a4a880d5ff64dce72e31a7217
groundnuty <groundnuty@gmail.com>
2022-05-29 19:45:02 +0200

    configured django_heroku

--- goodmovies/settings.py
+++ goodmovies/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,8 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = 'static/'
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+#STATIC_URL = 'static/'
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
