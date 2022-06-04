a635853b14066365aa5804d01673de39bd3a3c16
antnieszka <5757700+antnieszka@users.noreply.github.com>
2022-06-04 03:07:52 +0200

    Heroku deployment (#1)
    * fixed name of requirements.txt
    
    * fixed missing STATIC_ROOT variable
    
    * added missing to requirements.txt
    
    * configured django_heroku
    
    * added Procfile for heroku deployment
    
    Co-authored-by: groundnuty <groundnuty@gmail.com>

--- goodmovies/settings.py
+++ goodmovies/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+#STATIC_URL = 'static/'
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
