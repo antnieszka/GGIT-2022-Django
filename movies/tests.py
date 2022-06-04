2a2dbcca6b14f940c284c06c41850fd45e6ee10d
antnieszka <5757700+antnieszka@users.noreply.github.com>
2022-06-04 11:49:13 +0200

    Example tests

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,18 @@
+from datetime import datetime
 from django.test import TestCase
 
+from .models import Movie
+
 # Create your tests here.
+class TestMovies(TestCase):
+    def setUp(self):
+        Movie.objects.create(title="test", published_at=datetime.now())
+
+    def test_true(self):
+        assert True
+
+    def test_movie_count(self):
+        assert Movie.objects.count() == 1
+
+    def test_movie_get(self):
+        assert Movie.objects.get(title="test")
